bf54bdb12c193c76b34bcbf226a778e345b1892c
João Rodrigues <johnnyrod4569@gmail.com>
2024-05-24 10:35:04 +0200

    chore: Add function to randomise the game data received to index.js.

--- static/js/index.js
+++ static/js/index.js
@@ -6,12 +6,37 @@ document.querySelector('#burger').addEventListener('click',(e) => {
   nav.classList.toggle('show');
 });
 
+// Trigger the fetchGameData function when the page is loaded
 fetchGameData();
 function fetchGameData() {
     fetch('./static/js/game_data/game_data.json')
     .then(response => response.json())
     .then(function(game_data) {
-        console.log(game_data);
+
+        // Shuffle the order of the game data
+        let shuffledGameData = shuffleArray([...game_data]);
+        console.log(shuffledGameData);
     })
     .catch(error => console.error('Error:', error));
+}
+
+// Shuffle an array's elements order
+function shuffleArray(array) {
+    let currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+
+    }
+
+    return array;
 }
